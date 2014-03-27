b0b2bd5dddeb02d7c87c2225aec0b041dc422b9b
Dave Geffken <dave@cloudspace.com>
2014-03-26 20:09:28 -0400

    Node server working without passport option enabled

--- app/templates/lib/.env.js
+++ app/templates/lib/.env.js
@@ -1,19 +1,19 @@
 # 3rd Party Env Variables
 # Be sure your URLs on the 3rd Party App Settings are correct to your setup
 
-<% if props.facebook_client_id.present? && props.facebook_client_secret.present? %>
+<% if(props.facebook_client_id != "" && props.facebook_client_secret != ""){ %>
 FACEBOOK_CLIENT_ID=<%= props.facebook_client_id %>
 FACEBOOK_CLIENT_SECRET=<%= props.facebook_client_secret %>
-<% end %>
-<% if props.twitter_consumer_key.present? && props.twitter_consumer_secret.present? %>
+<% } %>
+<% if(props.twitter_consumer_key != "" && props.twitter_consumer_secret != ""){ %>
 TWITTER_CONSUMER_KEY=<%= props.twitter_consumer_key %>
 TWITTER_CONSUMER_SECRET=<%= props.twitter_consumer_secret %>
-<% end %>
-<% if props.google_client_id.present? && props.google_client_secret.present? %>
+<% } %>
+<% if(props.google_client_id != "" && props.google_client_secret != ""){ %>
 GOOGLE_CLIENT_ID=<%= props.google_client_id %>
 GOOGLE_CLIENT_SECRET=<%= props.google_client_secret %>
-<% end %>
-<% if props.github_client_id.present? && props.github_client_secret.present? %>
+<% } %>
+<% if(props.github_client_id != "" && props.github_client_secret != ""){ %>
 GITHUB_CLIENT_ID=<%= props.github_client_id %>
 GITHUB_CLIENT_SECRET=<%= props.github_client_secret %>
-<% end %>
+<% } %>
