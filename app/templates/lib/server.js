7aeb5251d59df162a3e449627da20d09c0bb5215
hkal <_@hkal.me>
2014-05-02 11:30:36 -0400

    remove bower execution

--- app/templates/lib/server.js
+++ app/templates/lib/server.js
@@ -6,47 +6,43 @@
  * Licensed under the <%= props.license %> license.
  */
 
-var exec = require('child_process').exec;
+var express = require('express')
+  , http = require('http')
+  , favicon = require('static-favicon')
+  , logger = require('morgan')
+  , cookieParser = require('cookie-parser')
+  , bodyParser = require('body-parser')<% if(usePassport) { %>
+  , passport = require('passport')
+  , env = require('node-env-file')<% } %>
+  ;
 
-exec('bower install', {cwd: __dirname + '/public'}, function(e) {
-  var express = require('express')
-    , http = require('http')
-    , favicon = require('static-favicon')
-    , logger = require('morgan')
-    , cookieParser = require('cookie-parser')
-    , bodyParser = require('body-parser')<% if(usePassport) { %>
-    , passport = require('passport')
-    , env = require('node-env-file')<% } %>
-    ;
+var app = express();
 
-  var app = express();
+// Basics
+app.set('port', process.env.PORT || 80);
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public/app'));
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.session({ secret: 'gobbledygook' }));
 
-  // Basics
-  app.set('port', process.env.PORT || 80);
-  app.set('view engine', 'ejs');
-  app.use(express.static(__dirname + '/public/app'));
-  app.use(favicon());
-  app.use(logger('dev'));
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded());
-  app.use(cookieParser());
-  app.use(express.session({ secret: 'gobbledygook' }));
+<% if(usePassport) { %>
+// Load process.env variables
+env(__dirname + '/.env');
 
-  <% if(usePassport) { %>
-  // Load process.env variables
-  env(__dirname + '/.env');
+// Passport
+require('./config/passport')(passport);
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 
-  // Passport
-  require('./config/passport')(passport);
-  app.use(passport.initialize());
-  app.use(passport.session()); // persistent login sessions
+// Setup routing
+app.use(app.router);
+require('./config/routes')(app, passport);
+<% } %>
 
-  // Setup routing
-  app.use(app.router);
-  require('./config/routes')(app, passport);
-  <% } %>
-
-  http.createServer(app).listen(app.get('port'), function() {
-    console.log("Express server listening on port " + app.get('port'));
-  });
+http.createServer(app).listen(app.get('port'), function() {
+  console.log("Express server listening on port " + app.get('port'));
 });
